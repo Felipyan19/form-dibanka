af024541ea851a6e6dbac4863f2070a5c0a41807
FelipyanCCG <felipyan199@gmail.com>
2023-10-26 21:00:54 -0500

    evaluation before send

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,171 +1,134 @@
-import { Formik, Field, Form as FormikForm } from 'formik';
+import { Formik, Form as FormikForm } from 'formik';
 import * as yup from 'yup';
-import { Row,  Card, Form, Button } from 'react-bootstrap';
+import { FormDefault } from '../FormDefault';
+import { Card, Button } from 'react-bootstrap';
 import { NewForm } from '../../Components/newForm';
 import imgCCG from '../../img/logo-ccgltda-1586537131.png';
 import formData from '../../Components/Form/formData';
 import { send_data } from '../../send_data';
 
 const FormCard = ({
-    selectedCampaña,
-    setSelectedCampaña,
-    selectedPagaduria,
-    setSelectedPagaduria,
-    formArray,
-    setFormArray,
-    setSelectedData,
-    backgroundImage,
-    setBackgroundImage,
-    text,
-    setText
+  selectedCampaña,
+  setSelectedCampaña,
+  selectedPagaduria,
+  setSelectedPagaduria,
+  formArray,
+  setFormArray,
+  setSelectedData,
+  backgroundImage,
+  setBackgroundImage,
+  text,
+  setText,
+  telefono,
+  setTelefono,
 }) => {
+  const schema = yup.object().shape({
+    formTelefono: yup.string().required('Este campo es obligatorio'),
+    formIdWolkvox: yup.string().required('Este campo es obligatorio'),
+    formPagaduria: yup.string().required('Este campo es obligatorio'),
+    formCampaña : yup.string().required('Este campo es obligatorio'),
+  });
 
+  const handleCampaignChange = (value) => {
+    const filterDataCampaign = value;
+    console.log(filterDataCampaign);
+    const dataForm = formData[selectedPagaduria][filterDataCampaign].data;
+    const configForm = formData[selectedPagaduria][filterDataCampaign].config;
+    setSelectedCampaña(filterDataCampaign);
 
-    const schema = yup.object().shape({
-        telefono: yup.string().required('Este campo es obligatorio'),
-        // Agrega más validaciones para otros campos según tus necesidades
-    });
+    if (formData[selectedPagaduria][filterDataCampaign] && dataForm) {
+      const initialFormArray = dataForm.map((form) => ({ ...form, value: '' }));
+      setFormArray(initialFormArray);
+      setBackgroundImage(configForm.backgroundImage);
+      setText(configForm.text);
+    } else {
+      setFormArray([]);
+    }
+  };
 
-    const handleCampañaChange = (e) => {
-        const filterdataCampaña = e.target.value;
-        const dataform = formData[selectedPagaduria][filterdataCampaña].data;
-        const configform = formData[selectedPagaduria][filterdataCampaña].config;
-        setSelectedCampaña(filterdataCampaña);
+  const handlePagaduriaChange = (value) => {
+    setSelectedPagaduria(value);
+  };
 
-        if (formData[selectedPagaduria][filterdataCampaña] && dataform) {
-            const initialFormArray = dataform.map((form) => ({ ...form, value: '' }));
-            setFormArray(initialFormArray);
-            setBackgroundImage(configform.backgroundImage);
-            setText(configform.text);
-        } else {
-            setFormArray([]);
-        }
-    };
-
-    const handlePagaduriaChange = (e) => {
-        const filterdata = e.target.value;
-        setSelectedPagaduria(filterdata);
-    };
-
-    const handleFieldChange = (fieldName, fieldValue) => {
-        const updatedFormArray = formArray.map((form) =>
-            form.title === fieldName ? { ...form, value: fieldValue } : form
-        );
-        setFormArray(updatedFormArray);
-    };
-
-    const isFormValid = () => {
-        // Verifica si todos los campos requeridos tienen un valor
-        return !formArray.some((form) => form.required && form.value === '');
-    };
-
-    const handleShowData = () => {
-        if (!isFormValid()) {
-            alert('Por favor, complete todos los campos obligatorios.');
-            return;
-        }
-
-        const data = {
-            Campaña: selectedCampaña,
-            Telefono: telefono,
-        };
-
-        formArray.forEach((form) => {
-            data[form.title] = form.value;
-        });
-        data.source = 'hoja 1';
-        console.log(data);
-        setSelectedData(data);
-        send_data(data);
-    };
-    return (
-        <Card.Body className="p-5">
-                        <img src={imgCCG} alt="logo" width="45%" className="img-fluid mb-5" />
-                        <Formik
-                            validationSchema={schema}
-                            onSubmit={handleShowData}
-                            initialValues={{
-                                telefono: '',
-                            }}
-                        >
-                            {({ handleSubmit, handleChange, values, touched, errors }) => (
-                                <FormikForm noValidate onSubmit={handleSubmit}>
-                                    <Form.Group as={Row} className="mb-3" controlId="formTelefono">
-                                        <Form.Label column style={{ whiteSpace: 'nowrap', overflow: 'hidden' }}>
-                                            Telefono
-                                        </Form.Label>
-                                        <Field
-                                            name="telefono"
-                                            type="text"
-                                            placeholder="Anonymous"
-                                            value={values.telefono}
-                                            onChange={handleChange}
-                                        />
-                                        {touched.telefono && errors.telefono ? (
-                                            <div className="text-danger">{errors.telefono}</div>
-                                        ) : null}
-                                    </Form.Group>
-                                    <Form.Group as={Row} className="mb-3" controlId="formWolkvox">
-                                        <Form.Label column style={{ whiteSpace: 'nowrap', overflow: 'hidden' }}>
-                                            ID Wolkvox
-                                        </Form.Label>
-                                        <Field
-                                            name="idWolkvox"
-                                            type="text"
-                                            placeholder="Anonymous"
-                                            value={values.idWolkvox}
-                                            onChange={handleChange}
-                                        />
-                                        {touched.idWolkvox && errors.idWolkvox ? (
-                                            <div className="text-danger">{errors.idWolkvox}</div>
-                                        ) : null}
-                                    </Form.Group>
-                                    <Form.Group as={Row} className="mb-3" controlId="formCampaña">
-                                        <Form.Label column style={{ whiteSpace: 'nowrap', overflow: 'hidden' }}>
-                                            Pagaduria
-                                        </Form.Label>
-                                        <Form.Select value={selectedPagaduria} onChange={handlePagaduriaChange}>
-                                            <option value="">Select a pagaduria</option>
-                                            {Object.keys(formData).map((Pagaduria) => (
-                                                <option key={Pagaduria} value={Pagaduria}>
-                                                    {Pagaduria}
-                                                </option>
-                                            ))}
-                                        </Form.Select>
-                                    </Form.Group>
-                                    <Form.Group as={Row} className="mb-3" controlId="formCampaña">
-                                        <Form.Label column style={{ whiteSpace: 'nowrap', overflow: 'hidden' }}>
-                                            Campaña
-                                        </Form.Label>
-                                        <Form.Select value={selectedCampaña} onChange={handleCampañaChange}>
-                                            <option value="">Select a campaign</option>
-                                            {Object.keys(formData[selectedPagaduria] || {}).map((campaña) => (
-                                                <option key={campaña} value={campaña}>
-                                                    {campaña}
-                                                </option>
-                                            ))}
-                                        </Form.Select>
-                                    </Form.Group>
-                                    {formArray.map((form) => (
-                                        <NewForm
-                                            key={form.title}
-                                            type={form.type}
-                                            title={form.title}
-                                            options={form.options}
-                                            value={form.value}
-                                            onValueChange={(fieldValue) =>
-                                                handleFieldChange(form.title, fieldValue)
-                                            }
-                                        />
-                                    ))}
-                                    <Button type="submit" className="btn mt-5" variant="primary">
-                                        Enviar Datos
-                                    </Button>
-                                </FormikForm>
-                            )}
-                        </Formik>
-                    </Card.Body>
+  const handleFieldChange = (fieldName, fieldValue) => {
+    const updatedFormArray = formArray.map((form) =>
+      form.title === fieldName ? { ...form, value: fieldValue } : form
     );
-}
+    setFormArray(updatedFormArray);
+  };
+
+  const isFormValid = () => {
+    return !formArray.some((form) => form.required && form.value === '');
+  };
+
+  const handleShowData = () => {
+    if (!isFormValid()) {
+      alert('Por favor, complete todos los campos obligatorios.');
+      return;
+    }
+
+    const data = {
+      campaign: selectedCampaña,
+      telefono: telefono,
+    };
+
+    formArray.forEach((form) => {
+      data[form.title] = form.value;
+    });
+    data.source = 'hoja 1';
+
+    setSelectedData(data);
+    send_data(data);
+  };
+
+  return (
+    <Card.Body className="p-5">
+      <img src={imgCCG} alt="logo" width="45%" className="img-fluid mb-5" />
+      <Formik
+        validationSchema={schema}
+        onSubmit={handleShowData}
+        initialValues={{
+            formTelefono: '',
+            formIdWolkvox: '',
+            formPagaduria: '',
+            formCampaña: '',
+        }}
+      >
+        {({ handleSubmit, handleChange, values, touched, errors }) => (
+          <FormikForm noValidate onSubmit={handleSubmit}>
+            <FormDefault
+              values={values}
+              handleChange={handleChange}
+              touched={touched}
+              errors={errors}
+              selectedPagaduria={selectedPagaduria}
+              setSelectedPagaduria={setSelectedPagaduria}
+              handlePagaduriaChange={handlePagaduriaChange}
+              formData={formData}
+              selectedCampaña={selectedCampaña}
+              setSelectedCampaña={setSelectedCampaña}
+              handleCampañaChange={handleCampaignChange}
+            />
+            {formArray.map((form) => (
+              <NewForm
+                key={form.title}
+                type={form.type}
+                title={form.title}
+                options={form.options}
+                value={form.value}
+                onValueChange={(fieldValue) =>
+                  handleFieldChange(form.title, fieldValue)
+                }
+              />
+            ))}
+            <Button type="submit" className="btn mt-5" variant="primary">
+              Enviar Datos
+            </Button>
+          </FormikForm>
+        )}
+      </Formik>
+    </Card.Body>
+  );
+};
 
 export { FormCard };
---
+++ src/Components/FormDefault/index.jsx
@@ -0,0 +1,80 @@
+import { Row, Form } from 'react-bootstrap';
+
+const FormDefault = ({
+  values,
+  handleChange,
+  touched,
+  errors,
+  selectedPagaduria,
+  handlePagaduriaChange,
+  formData,
+  selectedCampaña,
+  handleCampañaChange,
+}) => {
+  const renderOptions = (options) =>
+    Object.keys(options).map((option) => (
+      <option key={option} value={option}>
+        {option}
+      </option>
+    ));
+
+  const customHandleChange = (name, value) => {
+    handleChange(name)(value);
+  };
+
+  const renderFormGroup = (id, label, selectOptions) => (
+    <Form.Group as={Row} className="mb-3" controlId={id}>
+      <Form.Label column style={{ whiteSpace: 'nowrap', overflow: 'hidden' }}>
+        {label}
+      </Form.Label>
+      {!selectOptions && (
+        <div>
+          <Form.Control
+            name={id}
+            type="text"
+            placeholder="Anonymous"
+            value={values[id]}
+            onChange={(e) => customHandleChange(id, e.target.value)}
+            isValid={touched[id] && !errors[id]}
+          />
+        </div>
+      )}
+      {selectOptions && (
+        <div>
+          <Form.Select
+            name={id}
+            value={selectOptions.value || ''}
+            onChange={(e) => {
+              selectOptions.onChange(e.target.value);
+              customHandleChange(id, e.target.value);
+            }}
+            isValid={touched[id] && !errors[id]}
+          >
+            <option value="">Select a {label.toLowerCase()}</option>
+            {renderOptions(selectOptions.data)}
+          </Form.Select>
+        </div>
+      )}
+      {touched[id] && errors[id] && <div className="text-danger">{errors[id]}</div>}
+    </Form.Group>
+  );
+
+  return (
+    <>
+      {renderFormGroup('formTelefono', 'Telefono')}
+      {renderFormGroup('formIdWolkvox', 'ID Wolkvox')}
+      {renderFormGroup('formPagaduria', 'Pagaduria', {
+        value: selectedPagaduria,
+        onChange: handlePagaduriaChange,
+        data: formData,
+      })}
+      {renderFormGroup('formCampaña', 'Campaña', {
+        value: selectedCampaña,
+        onChange: handleCampañaChange,
+        data: selectedPagaduria ? formData[selectedPagaduria] : null,
+      })}
+    </>
+  );
+};
+
+export { FormDefault };
